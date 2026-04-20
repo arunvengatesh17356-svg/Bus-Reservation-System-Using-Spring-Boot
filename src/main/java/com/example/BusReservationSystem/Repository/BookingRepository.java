bfe3b51b3f8e7f4c31403141f2fa16524b725b42
Arun vengatesh <arunvengatesh17356@gmail.com>
2026-04-20 16:00:37 +0530

    Add method to find bookings by passenger number

--- src/main/java/com/example/BusReservationSystem/Repository/BookingRepository.java
+++ src/main/java/com/example/BusReservationSystem/Repository/BookingRepository.java
@@ -4,8 +4,9 @@ import com.example.BusReservationSystem.Entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
-    int countByBusNoAndDate(int busNo, LocalDate date);
     boolean existsByBusNoAndDateAndSeatNo(int busNo, LocalDate date, int seatNo);
+    List<Booking> findByPassengerNumber(String passengerNumber);
 }
